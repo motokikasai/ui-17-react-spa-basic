478f8c2af794559f653de446ec9c9c511f73b0b6
motokikasai <kmomoky@hotmail.com>
2020-03-19 15:31:56 +0100

    Install react & reactDOM and render headline

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,14 @@
 // The following line makes sure your styles are included in the project. Don't remove this.
 import "../styles/main.scss";
 import "regenerator-runtime/runtime";
+import React from "react";
+import ReactDOM from "react-dom";
 // Import any additional modules you want to include below \/
 
 // \/ All of your javascript should go here \/
+
+ReactDOM.render(
+  React.createElement("h1", {}, "My awesome contacts list"),
+  //   React.createElement("ul", {}, [React.createElement("li", {}, "Item ONE")]),
+  document.querySelector("#app")
+);
