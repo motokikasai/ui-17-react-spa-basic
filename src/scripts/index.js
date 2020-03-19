6e4e8aa5e5dfeb657d266c13d0c1678aedf53cd5
motokikasai <kmomoky@hotmail.com>
2020-03-19 21:58:46 +0100

    Add list item tentatively

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -7,8 +7,19 @@ import ReactDOM from "react-dom";
 
 // \/ All of your javascript should go here \/
 
-ReactDOM.render(
-  React.createElement("h1", {}, "My awesome contacts list"),
-  //   React.createElement("ul", {}, [React.createElement("li", {}, "Item ONE")]),
-  document.querySelector("#app")
-);
+const header = React.createElement("h1", {}, [
+  React.createElement("span", {}, "My"),
+  React.createElement("span", { style: { color: "hotpink" } }, " awesome"),
+  React.createElement("span", {}, " contact list")
+]);
+
+const list = React.createElement("ul", { style: { color: "mistyrose" } }, [
+  React.createElement("li", {}, [
+    React.createElement("p", {}, "Valentine Torp"),
+    React.createElement("a", { href: "#" }, "PHone: 555-2342"),
+    React.createElement("p", {}, ""),
+    React.createElement("a", {}, "Email: johnny@be.good")
+  ])
+]);
+
+ReactDOM.render([header, list], document.querySelector("#app"));
